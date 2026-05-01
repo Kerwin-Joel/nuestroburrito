c5618e8e3d023d9f64923a3fdef1889159143a81
Kerwin <joelss4029@gmail.com>
2026-04-30 23:50:36 -0500

    add theme version

--- src/components/shared/MapView.tsx
+++ src/components/shared/MapView.tsx
@@ -131,7 +131,7 @@ export default function MapView() {
         ref={setMapRef}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://carto.com">CARTO</a>'
         />
 
